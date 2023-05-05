dca237ac72383aae3f0a0ff5aa9360ba0511e43b
Eglebone <eglebone@gmail.com>
2023-05-06 05:12:10 +0600

    Notification System Introduced resolve #5

--- app/src/main/java/com/example/swiftslotz/MainActivity.java
+++ app/src/main/java/com/example/swiftslotz/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.swiftslotz;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createNotificationChannel();
         // Initialize the RecyclerView
         RecyclerView appointmentsRecyclerView = findViewById(R.id.appointmentsRecyclerView);
 
@@ -32,5 +41,39 @@ public class MainActivity extends AppCompatActivity {
         // Set the layout manager and adapter for the RecyclerView
         appointmentsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         appointmentsRecyclerView.setAdapter(appointmentsAdapter);
+
+        FloatingActionButton appointmentButton = findViewById(R.id.addAppointmentButton);
+
+        appointmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNotification();
+            }
+        });
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "AppointmentChannel";
+            String description = "Channel for appointment notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("appointmentChannel", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void showNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = new Notification.Builder(this, "appointmentChannel")
+                .setContentTitle("Appointment Button")
+                .setContentText("Button added")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .build();
+
+        notificationManager.notify(1, notification);
     }
 }
