478ad315306d2c81808acc44676f5b78cabc47a7
Arn <arnarnsde@gmail.com>
2023-05-06 04:06:21 +0600

    Added Adapter Class for Dummy Data resolve #3

---
+++ app/src/main/java/com/example/swiftslotz/Appointment.java
@@ -0,0 +1,38 @@
+package com.example.swiftslotz;
+
+public class Appointment {
+    private String title;
+    private String date;
+    private String time;
+
+    public Appointment(String title, String date, String time) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
+
--- app/src/main/java/com/example/swiftslotz/MainActivity.java
+++ app/src/main/java/com/example/swiftslotz/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.swiftslotz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the RecyclerView
+        RecyclerView appointmentsRecyclerView = findViewById(R.id.appointmentsRecyclerView);
+
+        // Create a list of dummy appointments
+        List<Appointment> appointments = new ArrayList<>();
+        appointments.add(new Appointment("Appointment 1", "2023-06-05", "10:00 AM"));
+        appointments.add(new Appointment("Appointment 2", "2023-06-07", "02:00 PM"));
+        appointments.add(new Appointment("Appointment 3", "2023-06-12", "11:00 AM"));
+        appointments.add(new Appointment("Appointment 4", "2023-06-15", "04:00 PM"));
+
+        // Create an instance of AppointmentsAdapter
+        AppointmentsAdapter appointmentsAdapter = new AppointmentsAdapter(appointments);
+
+        // Set the layout manager and adapter for the RecyclerView
+        appointmentsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        appointmentsRecyclerView.setAdapter(appointmentsAdapter);
     }
 }
