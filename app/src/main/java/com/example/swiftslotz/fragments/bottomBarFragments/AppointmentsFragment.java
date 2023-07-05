e862b2ce56e3796c90769440fd7eda68ac4aaafe
Eglebone <eglebone@gmail.com>
2023-07-05 20:03:42 +0600

    c12

--- app/src/main/java/com/example/swiftslotz/fragments/bottomBarFragments/AppointmentsFragment.java
+++ app/src/main/java/com/example/swiftslotz/fragments/bottomBarFragments/AppointmentsFragment.java
@@ -1,66 +1,89 @@
 package com.example.swiftslotz.fragments.bottomBarFragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.swiftslotz.fragments.pageFragments.AddAppointmentFragment;
+import com.example.swiftslotz.fragments.pageFragments.ModifyAppointmentFragment;
+import com.example.swiftslotz.utilities.Appointment;
+import com.example.swiftslotz.utilities.AppointmentManager;
+import com.example.swiftslotz.utilities.AppointmentsAdapter;
 import com.example.swiftslotz.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AppointmentsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AppointmentsFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AppointmentsFragment extends Fragment implements AppointmentsAdapter.OnAppointmentInteractionListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AppointmentsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AppointmentsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AppointmentsFragment newInstance(String param1, String param2) {
-        AppointmentsFragment fragment = new AppointmentsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private List<Appointment> appointments;
+    private AppointmentsAdapter appointmentsAdapter;
+    private RecyclerView appointmentsRecyclerView;
+    private AppointmentManager appointmentManager;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page1, container, false);
+        View view = inflater.inflate(R.layout.fragment_appointments, container, false);
+
+        appointmentsRecyclerView = view.findViewById(R.id.appointmentsRecyclerView);
+        appointments = new ArrayList<>();
+        appointmentsAdapter = new AppointmentsAdapter(appointments, this);
+        appointmentsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        appointmentsRecyclerView.setAdapter(appointmentsAdapter);
+
+        appointmentManager = new AppointmentManager(getActivity(), appointments, appointmentsAdapter);
+
+        FloatingActionButton appointmentButton = view.findViewById(R.id.addAppointmentButton);
+        appointmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddAppointmentFragment addAppointmentFragment = new AddAppointmentFragment();
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.content_frame, addAppointmentFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        appointmentManager.fetchDataFromDatabase();
+    }
+
+    @Override
+    public void onEditAppointment(Appointment appointment) {
+        startModifyAppointmentFragment(appointment.getId(), appointment.getTitle(), appointment.getDate(), appointment.getTime(), appointment.getDetails(), appointment.getKey());
+    }
+
+    @Override
+    public void onDeleteAppointment(Appointment appointment) {
+        appointmentManager.deleteAppointment(appointment);
+    }
+
+    public void startModifyAppointmentFragment(int appointmentId, String title, String date, String time, String details, String key) {
+        ModifyAppointmentFragment modifyAppointmentFragment = new ModifyAppointmentFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("appointment_id", appointmentId);
+        bundle.putString("appointment_title", title);
+        bundle.putString("appointment_date", date);
+        bundle.putString("appointment_time", time);
+        bundle.putString("appointment_details", details);
+        bundle.putString("appointment_key", key);
+        modifyAppointmentFragment.setArguments(bundle);
+
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, modifyAppointmentFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
