7327c364d113152144c706a2f0114f8ff7adcd58
Arn <arnarnsde@gmail.com>
2023-10-10 21:47:11 +0600

    modal implemented resolve #26

--- app/src/main/java/com/example/swiftslotz/utilities/UserAdapter.java
+++ app/src/main/java/com/example/swiftslotz/utilities/UserAdapter.java
@@ -1,8 +1,13 @@
 package com.example.swiftslotz.utilities;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,10 +43,11 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Clicked on: " + user.getUsername(), Toast.LENGTH_SHORT).show();
+                showUserDetailModal(v.getContext(), user);
             }
         });
     }
+
 
     @Override
     public int getItemCount() {
@@ -56,5 +62,62 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
             usernameTextView = view.findViewById(R.id.usernameTextView);
         }
     }
+
+    private void showUserDetailModal(Context context, User user) {
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.user_detail_modal);
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        // Set the dimensions of the dialog
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = (int)(width * 0.95);  // Set width to 90% of screen width
+        lp.height = (int)(height * 0.4);  // Set height to 70% of screen height
+        dialog.getWindow().setAttributes(lp);
+
+        // Initialize TextViews
+        TextView firstNameTextView = dialog.findViewById(R.id.firstNameTextView);
+        TextView lastNameTextView = dialog.findViewById(R.id.lastNameTextView);
+        TextView usernameTextView = dialog.findViewById(R.id.usernameTextView);
+        TextView emailTextView = dialog.findViewById(R.id.emailTextView);
+        TextView phoneTextView = dialog.findViewById(R.id.phoneTextView);
+        TextView companyTextView = dialog.findViewById(R.id.companyTextView);
+        TextView addressTextView = dialog.findViewById(R.id.addressTextView);
+
+        // Initialize Buttons
+        Button getAppointmentButton = dialog.findViewById(R.id.getAppointmentButton);
+        Button goBackButton = dialog.findViewById(R.id.goBackButton);
+
+        // Set user details
+        firstNameTextView.setText("First Name: " + user.getFirstName());
+        lastNameTextView.setText("Last Name: " + user.getLastName());
+        usernameTextView.setText("Username: " + user.getUsername());
+        emailTextView.setText("Email: " + user.getEmail());
+        phoneTextView.setText("Phone: " + user.getPhone());
+        companyTextView.setText("Company: " + user.getCompany());
+        addressTextView.setText("Address: " + user.getAddress());
+
+        // Set button click listeners
+        getAppointmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement your logic here for getting an appointment
+                dialog.dismiss();
+            }
+        });
+
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
 }
 
