8d441cd203094a3044784e064ed6433300490d09
Arn <arnarnsde@gmail.com>
2023-11-14 10:51:41 +0600

    Profile updated with photo

--- app/src/main/java/com/example/swiftslotz/fragments/bottomBarFragments/ProfileFragment.java
+++ app/src/main/java/com/example/swiftslotz/fragments/bottomBarFragments/ProfileFragment.java
@@ -1,11 +1,14 @@
 package com.example.swiftslotz.fragments.bottomBarFragments;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.swiftslotz.BuildConfig;
 import com.example.swiftslotz.R;
 import com.example.swiftslotz.activities.LogoutActivity;
@@ -22,25 +26,25 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ProfileFragment extends Fragment {
 
-    private TextView firstName;
-    private TextView lastName;
-    private TextView username;
-    private TextView email;
-    private TextView phone;
-    private TextView occupation;
-    private TextView address;
+    private TextView firstName, lastName, username, email, phone, occupation, address;
+    private ImageView profileImage;
+    private Button uploadPhotoButton, logoutButton;
 
     private FirebaseAuth mAuth;
     private DatabaseReference userDb;
+    private StorageReference storageReference;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // Initialize UI components
         firstName = view.findViewById(R.id.user_firstName);
         lastName = view.findViewById(R.id.user_lastName);
         username = view.findViewById(R.id.user_username);
@@ -48,26 +52,32 @@ public class ProfileFragment extends Fragment {
         phone = view.findViewById(R.id.user_phone);
         occupation = view.findViewById(R.id.user_occupation);
         address = view.findViewById(R.id.user_address);
+        profileImage = view.findViewById(R.id.profile_image);
+        uploadPhotoButton = view.findViewById(R.id.upload_photo_button);
+        logoutButton = view.findViewById(R.id.logout_button);
 
         mAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
         if (mAuth.getCurrentUser() != null) {
             String userId = mAuth.getCurrentUser().getUid();
             userDb = FirebaseDatabase.getInstance(BuildConfig.FIREBASE_DATABASE_URL).getReference("users").child(userId);
-
             fetchUserDetails();
         } else {
             Toast.makeText(getActivity(), "User not signed in", Toast.LENGTH_SHORT).show();
         }
 
-        Button logoutButton = view.findViewById(R.id.logout_button);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), LogoutActivity.class);
-                startActivity(intent);
-            }
+        uploadPhotoButton.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 123);
         });
 
+        logoutButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), LogoutActivity.class);
+            startActivity(intent);
+        });
 
         return view;
     }
@@ -77,6 +87,7 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
+                    // Set user details from the database to the TextViews
                     firstName.setText(dataSnapshot.child("firstName").getValue(String.class));
                     lastName.setText(dataSnapshot.child("lastName").getValue(String.class));
                     username.setText(dataSnapshot.child("username").getValue(String.class));
@@ -84,6 +95,12 @@ public class ProfileFragment extends Fragment {
                     phone.setText(dataSnapshot.child("phone").getValue(String.class));
                     occupation.setText(dataSnapshot.child("occupation").getValue(String.class));
                     address.setText(dataSnapshot.child("address").getValue(String.class));
+
+                    // Load profile image if it exists in the database
+                    if (dataSnapshot.hasChild("profileImageUrl")) {
+                        String imageUrl = dataSnapshot.child("profileImageUrl").getValue(String.class);
+                        Glide.with(ProfileFragment.this).load(imageUrl).into(profileImage);
+                    }
                 } else {
                     Toast.makeText(getActivity(), "Failed to load user details", Toast.LENGTH_SHORT).show();
                 }
@@ -91,8 +108,34 @@ public class ProfileFragment extends Fragment {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(getActivity(), "Failed to load user details", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Failed to load user details: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123 && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            uploadImageToFirebase(imageUri);
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        if (mAuth.getCurrentUser() != null) {
+            String userId = mAuth.getCurrentUser().getUid();
+            StorageReference fileRef = storageReference.child("profileImages/" + userId + ".jpg");
+            fileRef.putFile(imageUri).addOnSuccessListener(taskSnapshot -> fileRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                String imageUrl = uri.toString();
+                userDb.child("profileImageUrl").setValue(imageUrl)
+                        .addOnSuccessListener(aVoid -> {
+                            Glide.with(ProfileFragment.this).load(imageUrl).into(profileImage);
+                            Toast.makeText(getActivity(), "Profile image updated", Toast.LENGTH_SHORT).show();
+                        })
+                        .addOnFailureListener(e -> Toast.makeText(getActivity(), "Failed to update image URL", Toast.LENGTH_SHORT).show());
+            })).addOnFailureListener(e -> Toast.makeText(getActivity(), "Failed to upload image", Toast.LENGTH_SHORT).show());
+        }
     }
 }
